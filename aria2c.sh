c3bbf4cd4cf88c20e97b4d4b20e4bbb3f84a0093
zgray <cnzgray@qq.com>
2017-11-22 21:21:33 +0800

    update

--- aria2c.sh
+++ aria2c.sh
@@ -8,6 +8,13 @@ else
   CADDY_FILE=/usr/local/caddy/Caddyfile
 fi
 
+touch /root/conf/aria2.session
+
+if [[ ! -e /root/conf/aria2.conf ]]
+then
+  cp /root/conf/aria2.conf.default /config/aria2.conf
+fi
+
 if [ "$SSL" = "true" ]; then
 echo "Start aria2 with secure config"
 
@@ -23,8 +30,10 @@ echo "Start aria2 with secure config"
 else
 echo "Start aria2 with standard mode"
 /usr/bin/aria2c --conf-path="/root/conf/aria2.conf" -D \
---enable-rpc --rpc-listen-all \
-&& caddy -quic --conf ${CADDY_FILE}
+ --input-file=/root/conf/aria2.session \
+ --save-session=/root/conf/aria2.session \
+ --enable-rpc --rpc-listen-all \
+ && caddy -quic --conf ${CADDY_FILE}
 fi
 
 
